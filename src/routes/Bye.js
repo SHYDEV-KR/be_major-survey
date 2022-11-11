4386be118b08e604d40c623a7722da23468fb0a9
SHY <ekdmbzzang@naver.com>
2022-11-11 21:11:09 +0900

    링크 복사기능 추가

--- src/routes/Bye.js
+++ src/routes/Bye.js
@@ -1,7 +1,23 @@
+import { Button, ButtonGroup } from "@chakra-ui/button";
 import { Image } from "@chakra-ui/image";
 import { Heading, HStack, Text, VStack } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
+import { FaLink } from "react-icons/fa";
 
 const Bye = () => {
+	const toast = useToast();
+	const copyUrl = async () => {
+		const url = "https://shydev-kr.github.io/be_major-survey/";
+		await navigator.clipboard.writeText(url);
+		toast({
+			title: "링크가 복사되었습니다!",
+			description: "설문조사를 널리널리 공유해주세요😊",
+			status: "success",
+			duration: 9000,
+			isClosable: true,
+		});
+	};
+
 	return (
 		<VStack minH={"100vh"}>
 			<VStack minH={"100vh"} justifyContent={"center"} px={10} maxW={"100vw"}>
@@ -11,7 +27,10 @@ const Bye = () => {
 				<Heading textAlign={"center"} pb={"10"}>
 					답변해주셔서 감사합니다. <br />늘 여러분의 커리어를 응원할게요!
 				</Heading>
-				<Text pb={20}>활발한 공유는 통계에 이롭습니다 :)</Text>
+				<Text pb={10}>활발한 공유는 통계에 이롭습니다 :)</Text>
+				<Button colorScheme="linkedin" rightIcon={<FaLink />} onClick={copyUrl}>
+					링크 복사하기
+				</Button>
 			</VStack>
 		</VStack>
 	);
